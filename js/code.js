d8628fae826b73c212ffb5852f661b4e4b4d7eef
2011.peswainer.1996@gmail.com <61250382>
2017-07-06 08:45:48 -0600

    nuevo codigo
    Se añade la opción de foto de perfil del usuario y una galeria de fotos
    para la vendedores, estas fotos son del local

--- js/code.js
+++ js/code.js
@@ -209,3 +209,17 @@ for (var i=0;i<hearts.length;i++) {
 }
 
 
+
+
+/**funcion de la galeria del vendedor**/
+$(function(){
+	var	btn = $(".slider__btn");
+	
+	btn.on("click",function(){
+		$(".slider__item").first().clone().appendTo(".slider");
+		$(".slider__image").first().css({transform: "rotateX(-180deg)", opacity: 0});
+		setTimeout(function(){
+			$(".slider__item").first().remove();
+		},200);
+	});
+});
