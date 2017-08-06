5b76bf848bf2b414067e7a27e685ac912eb135d4
2011.peswainer.1996@gmail.com <61250382>
2017-08-06 14:12:18 -0600

    Nuevo codigo
    Se agrega codigo en la parte del mapa de la ubicación, guardar en cokies
    y otras funciones

--- js/RegistroUsuario.js
+++ js/RegistroUsuario.js
@@ -1,10 +1,156 @@
-document.querySelector('#btn-ingresar').addEventListener('click', verificarLogin);
-function verificarLogin() {
-    alert("hola");
-
-
- }
-
+Usuarios = [];
+var platitud;
+var plongitud;
+document.querySelector('#btnIngresar').addEventListener('click', capturar);
 
 
 
+function validarPasswords(p1, p2, valor, pEdad) {
+    var edad = parseInt(pEdad);
+    var espacios = false;
+    var cont = 0;
+    if (p1.length <= 0 || p2.length <= 0) {
+        alert("Los campos de la password no pueden quedar vacios");
+        return false;
+    }
+    if (!/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)) {
+        alert("La dirección de email es incorrecta!.");
+        return false;
+
+    }
+    if (p1 != p2) {
+        alert("Las passwords deben de coincidir");
+        return false;
+    }
+    while (!espacios && (cont < document.querySelector('#txtContrasenna1').value.length)) {
+        if (p1.charAt(cont) == " ")
+            espacios = true;
+        cont++;
+    }
+
+    if (espacios) {
+        alert("La contraseña no puede contener espacios en blanco");
+        return false;
+    }
+    if (edad <= 0) {
+        alert("Ingresa una edad correcta");
+    }
+      
+    return true;
+}
+
+function capturar() {
+
+    var nombre = document.querySelector('#txtNombre').value,
+        apellidoPaterno = document.querySelector('#txtApellidoPaterno').value,
+        apellidoMaterno = document.querySelector('#txtApellidoMaterno').value,
+        email = document.querySelector('#txtEmail').value,
+        edad = parseInt(document.querySelector('#txtEdad').value),
+        latitud = platitud,
+        longitud = plongitud,
+        usuario = document.querySelector('#txtUsuario').value,
+        contrasenna1 = document.querySelector('#txtContrasenna1').value,
+        contrasenna2 = document.querySelector('#txtContrasenna2').value;
+    if (validarPasswords(contrasenna1, contrasenna2, email, edad) == true) {
+        addUsuarios(nombre, apellidoPaterno, apellidoMaterno, email, edad, latitud, longitud, usuario, contrasenna1);
+    }
+}
+
+function addUsuarios(pNombre, pApellidoPaterno, pApellidoMaterno, pEmil, pEdad, pLatitud, pLongitud, pUsuario, pContrasenna) {
+
+    var newUsuario = {
+        nombre: pNombre,
+        apellidoPaterno: pApellidoPaterno,
+        apellidoMaterno: pApellidoMaterno,
+        email: pEmil,
+        edad: pEdad,
+        latitud: pLatitud,
+        longitud: pLongitud,
+        usuario: pUsuario,
+        contrasenna: pContrasenna
+    };
+
+    console.log(newUsuario);
+    Usuarios.push(newUsuario);
+    guardarLista(Usuarios);
+
+
+}
+
+function guardarLista(NuevoUsuario) {
+    localStorage.setItem('usuarios', JSON.stringify(NuevoUsuario));
+}
+
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 10.0000000,
+            lng: -84.0000000
+        },
+        zoom: 13
+    });
+    var input = document.getElementById('searchInput');
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29)
+    });
+
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Ingrese una dirección correcta");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+        marker.setIcon(({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+
+        //Location details
+        for (var i = 0; i < place.address_components.length; i++) {
+            if (place.address_components[i].types[0] == 'postal_code') {
+                document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
+            }
+            if (place.address_components[i].types[0] == 'country') {
+                document.getElementById('country').innerHTML = place.address_components[i].long_name;
+            }
+        }
+        platitud = place.geometry.location.lat();
+        plongitud = place.geometry.location.lng();
+        document.getElementById('location').innerHTML = place.formatted_address;
+    });
+}
--- js/login.js
+++ js/login.js
@@ -1,23 +1,64 @@
 var Usuarios = [];
 document.querySelector('#btn-ingresar').addEventListener('click', verificarLogin);
 
+window.addEventListener('load', cargar, false);
+
+function cargar() {
+    var usu = document.getElementById('txt-usuario');
+    var contra = document.getElementById('txt-contrasena');
+    usu.value = obtenerCookie("USUARIO");
+    contra.value = obtenerCookie("CONTRASEÑA");
+}
+
 function verificarLogin() {
-var usuario = document.querySelector('#txt-usuario').value;
-var telefono = document.querySelector('#txt-contrasena').value;
+    cargarUsuarios();
+    var usuario = document.querySelector('#txt-usuario').value;
+    var contra = document.querySelector('#txt-contrasena').value;
+    var check = document.getElementById('input-checkbox-loginc').checked;
 
- }
+    for (i = 0; i < Usuarios.length; i++) {
+        if (Usuarios[i].usuario == usuario && Usuarios[i].contrasenna == contra) {
+            if (check == true) {
+                crearCookieuSUARIO("USUARIO", usuario, 900);
+                crearCookieuSUARIO("CONTRASEÑA", contra, 900);
+            }
+            window.location = "index.html?"+usuario;
+        } else {
+            alert("no son iguales");
+        }
 
-function cargarUsuarios(){    
-     var listaUsuarios = localStorage.getItem('lisOfPeople');
-    if (temList != null) {
+    }
+}
 
-        Usuarios = JSON.parse(temList);
+function crearCookieuSUARIO(clave, valor, diasexpiracion) {
+    var d = new Date();
+    d.setTime(d.getTime() + (diasexpiracion * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = clave + "=" + valor + "; " + expires;
+}
+
+function obtenerCookie(clave) {
+    var name = clave + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function cargarUsuarios() {
+    var listaUsuarios = localStorage.getItem('usuarios');
+    if (listaUsuarios != null) {
+
+        Usuarios = JSON.parse(listaUsuarios);
     } else {
         Usuarios = [];
         alert("No hay registros en el local storange");
     }
-    alert(temList.length);
-    if (temList.length === 2){
+    alert(listaUsuarios.length);
+    if (listaUsuarios.length === 2) {
         alert("No hay registros en el local storange");
     }
     return Usuarios;
